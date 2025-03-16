34ab356977daa2fc45f80179d8bbedab40271207
Paweł Kołodziej <pawelkolodziej81@gmail.com>
2025-03-16 17:38:25 +0100

    feat: todo edit button added

--- src/components/todo.ts
+++ src/components/todo.ts
@@ -1,61 +1,83 @@
 export class TodoComponent {
-    #listEl: HTMLElement | undefined;
-    #inputEl: HTMLTextAreaElement | undefined;
+  #listEl: HTMLElement | undefined;
+  #inputEl: HTMLTextAreaElement | undefined;
 
-    mount(targetEl: HTMLElement) {
+  mount(targetEl: HTMLElement) {
+    targetEl.classList.add('todo');
 
-        targetEl.classList.add('todo');
+    this.#listEl = document.createElement('ul');
+    this.#listEl.classList.add('todo-list');
 
-        this.#listEl = document.createElement('ul');
-        this.#listEl.classList.add('todo-list');
+    targetEl.appendChild(this.#listEl);
 
-        targetEl.appendChild(this.#listEl);
+    const footerEl = document.createElement('form');
+    targetEl.appendChild(footerEl);
 
-        const footerEl = document.createElement('form');
-        targetEl.appendChild(footerEl);
+    this.#inputEl = document.createElement('textarea');
+    footerEl.appendChild(this.#inputEl);
 
-        this.#inputEl = document.createElement('textarea');
-        footerEl.appendChild(this.#inputEl);
+    const addButtonEl = document.createElement('button');
+    addButtonEl.textContent = 'Dodaj'
 
-        const addButtonEl = document.createElement('button');
-        addButtonEl.textContent = 'Dodaj'
+    addButtonEl.addEventListener('click', (evt) => {
+      this.addItem()
+      evt.preventDefault()
+    })
 
-        addButtonEl.addEventListener('click', (evt) => {
-            this.addItem()
-            evt.preventDefault()
-        })
+    footerEl.appendChild(addButtonEl);
+    footerEl.classList.add('todo-footer');
+  }
 
-        footerEl.appendChild(addButtonEl);
-        footerEl.classList.add('todo-footer');
-    }
+  addItem(extText?: string) {
+    if (!this.#inputEl || !this.#listEl) throw new Error('Component probaly not initialized!');
 
-    addItem(extText?: string) {
-        if (!this.#inputEl || !this.#listEl) throw new Error('Component probaly not initialized!');
+    const itemEl = document.createElement('li')
+    itemEl.classList.add('todo-item');
 
-        const itemEl = document.createElement('li')
-        itemEl.classList.add('todo-item');
+    const checkEl = document.createElement('input');
+    checkEl.type = 'checkbox'
+    checkEl.addEventListener('change', () => {
+      itemEl.classList.toggle('todo-item-done');
+    });
 
-        const checkEl = document.createElement('input');
-        checkEl.type = 'checkbox'
-        checkEl.addEventListener('change', () => {
-            itemEl.classList.toggle('todo-item-done');
-        });
+    const textEl = document.createElement('div');
+    textEl.textContent = extText ? extText : this.#inputEl?.value ?? null
+    textEl.classList.add('todo-item-text')
 
-        const textEl = document.createElement('div');
-        textEl.textContent = extText ? extText : this.#inputEl?.value ?? null
-        textEl.classList.add('todo-item-text')
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Usuń";
+    deleteButton.addEventListener("click", () => {
+      this.#listEl!.removeChild(itemEl);
+    });
 
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Usuń";
-        deleteButton.addEventListener("click", () => {
-            this.#listEl!.removeChild(itemEl);
-        });
+    const editButton = document.createElement("button");
+    editButton.textContent = "Edytuj";
+    editButton.dataset.mode = "readonly";
 
-        itemEl.appendChild(checkEl)
-        itemEl.appendChild(textEl)
-        itemEl.appendChild(deleteButton)
+    let textEdit: HTMLTextAreaElement;
+    editButton.addEventListener("click", () => {
+      if (editButton.dataset.mode === "readonly") {
+        textEdit = document.createElement("textarea")
+        textEdit.value = textEl.textContent ?? "";
+        checkEl.after(textEdit);
+        textEl.classList.add("hidden");
+        editButton.dataset.mode = "editing";
+        editButton.textContent = "Zapisz";
+      } else {
+        textEl.textContent = textEdit.value;
+        textEl.classList.remove("hidden");
+        itemEl.removeChild(textEdit);
+        editButton.dataset.mode = "readonly";
+        editButton.textContent = "Edytuj";
+      }
+    });
 
-        this.#inputEl.value = ''
-        this.#listEl.appendChild(itemEl);
-    }
+    itemEl.appendChild(checkEl)
+    itemEl.appendChild(textEl)
+    itemEl.appendChild(deleteButton)
+    itemEl.appendChild(editButton)
+
+    this.#inputEl.value = ''
+    this.#listEl.appendChild(itemEl);
+  }
 }
